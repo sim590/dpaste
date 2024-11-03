7919ea9cb41fdffc488c4cf5ccbf38966b0189cc
Simon Désaulniers <sim.desaulniers@gmail.com>
2024-11-03 09:30:20 -0500

    http_client: use the patched curlpp header file

--- src/http_client.cpp
+++ src/http_client.cpp
@@ -22,7 +22,7 @@
 
 #include <curlpp/cURLpp.hpp>
 #include <curlpp/Easy.hpp>
-#include <curlpp/Options.hpp>
+#include "curlpp/Options.hpp"
 #include <curlpp/Exception.hpp>
 #include <curlpp/Infos.hpp>
 #include <json.hpp>
